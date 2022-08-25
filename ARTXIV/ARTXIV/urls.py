6d5af579c0b762df9250a118b58f094c73a85407
Sorao-high <66772476+Sorao-high@users.noreply.github.com>
2022-08-25 17:40:22 +0900

    Update urls.py
    追加
    18>>from django.conf import settings 
    19>>from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns 
    
    28>>if settings.DEBUG:
    29>>    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

--- ARTXIV/ARTXIV/urls.py
+++ ARTXIV/ARTXIV/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings 
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns 
 from artxivs.views import top
 
 urlpatterns = [
@@ -24,4 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 #hello shimo-san
