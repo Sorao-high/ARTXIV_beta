d3d61317ae2a67537bf51d28809a649965c96ebd
mamo-shimo <95067968+mamo-shimo@users.noreply.github.com>
2022-08-25 18:59:15 +0900

    Update views.py
    追加
    2>>redirect
    16>>request.FILES
    18>>commit=Falseを削除
    19>>str
    26>>artxiv_id=0

--- ARTXIV/artxivs/views.py
+++ ARTXIV/artxivs/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from artxivs.models import Artxiv
 from artxivs.forms import ArtxivForm
 
@@ -13,17 +13,17 @@ def top(request):
 @login_required
 def artxiv_new(request):
     if request.method == 'POST':
-        form = ArtxivForm(request.POST)
+        form = ArtxivForm(request.POST, request.FILES)
         if form.is_valid():
-            artxiv = form.save(commit=False)
-            artxiv.contributor_name = request.user
+            artxiv = form.save()
+            artxiv.contributor_name = str(request.user)
             artxiv.save()
             return redirect(artxiv_detail, artxiv_id=artxiv.pk)
     else:
         form = ArtxivForm()
     return render(request, "artxivs/artxiv_new.html", {'form': form})
 
-def artxiv_detail(request, artxiv_id):
+def artxiv_detail(request, artxiv_id=0):
     artxiv = get_object_or_404(Artxiv, pk=artxiv_id)
     return render(request, 'artxivs/artxiv_detail.html', {'artxiv':artxiv})
 
